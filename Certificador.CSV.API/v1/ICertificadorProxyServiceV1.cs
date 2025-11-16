346c0e296568d61d16ab79064deafb7bf4fdb2d5
SharonCanellon <canellonsharon2620@gmail.com>
2025-11-16 16:46:46 -0600

    Declaracion de los metodos

---
+++ Certificador.CSV.API/v1/ICertificadorProxyServiceV1.cs
@@ -0,0 +1,58 @@
+﻿using Certificador.DTO.Api;
+using Certificador.DTO.Api.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Certificador.CSV.Api.v1
+{
+    // Define el nombre del contrato de servicio SOAP.
+    [ServiceContract(Name = "CertificadorService")]
+    public interface ICertificadorProxyServiceV1
+    {
+        //Metodos para Cliente
+
+        [OperationContract(Name = "ObtenerClientes")]
+        Task<ResponseDTO<List<ClienteAPIDTO>>> ObtenerClientes();
+
+        [OperationContract(Name = "ObtenerClientePorId")]
+        Task<ResponseDTO<ClienteAPIDTO>> ObtenerClientePorId(int id);
+
+        [OperationContract(Name = "CrearCliente")]
+        Task<ResponseDTO<ClienteAPIDTO>> CrearCliente(ClienteAPIDTO cliente);
+
+        [OperationContract(Name = "EditarCliente")]
+        Task<ResponseDTO<ClienteAPIDTO>> EditarCliente(ClienteAPIDTO cliente);
+
+        [OperationContract(Name = "EliminarCliente")]
+        Task<ResponseDTO<ClienteAPIDTO>> EliminarCliente(int id);
+
+        //Metodos para Factura
+
+        [OperationContract(Name = "ObtenerFacturas")]
+        Task<ResponseDTO<List<FacturaDTO>>> ObtenerFacturas();
+
+        [OperationContract(Name = "ObtenerFacturaPorId")]
+        Task<ResponseDTO<FacturaDTO>> ObtenerFacturaPorId(int id);
+
+        [OperationContract(Name = "CrearFactura")]
+        Task<ResponseDTO<FacturaDTO>> CrearFactura(FacturaDTO factura);
+
+        [OperationContract(Name = "EditarFactura")]
+        Task<ResponseDTO<FacturaDTO>> EditarFactura(FacturaDTO factura);
+
+        [OperationContract(Name = "EliminarFactura")]
+        Task<ResponseDTO<FacturaDTO>> EliminarFactura(int id);
+
+        //Metodos para Factura Caertificada
+
+        [OperationContract(Name = "ObtenerFacturasCertificadas")]
+        Task<ResponseDTO<List<FacturaCertificadaDTO>>> ObtenerFacturasCertificadas();
+
+        [OperationContract(Name = "ObtenerFacturaCertificadaPorId")]
+        Task<ResponseDTO<FacturaCertificadaDTO>> ObtenerFacturaCertificadaPorId(int id);
+    }
+}
